dcd97493d7def201e3df21656e6ee036c027356c
User <User@LAPTOP-QTD9V40B>
2025-10-20 01:33:26 +0300

    Добавьте файлы проекта.

---
+++ task_2_/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2_
+{
+    public class BankAccount
+
+    {
+        // поля классф
+        private string owner;
+        private decimal balance;
+
+    
+        // доступ к полям класса
+        public string Owner
+        {
+            get => owner;
+            set => owner = value;
+        }
+        public decimal Balance
+        {
+            get => balance;
+        }
+        // конструктор
+        public BankAccount(string owner, decimal balance)
+        {
+            this.owner = owner;
+            this.balance = balance;
+        }
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+            Console.WriteLine($"{owner}, баланс: {balance}");
+
+        }
+        public void Withdraw(decimal amount)
+        {
+            balance -= amount;
+            Console.WriteLine($"{owner}, баланс: {balance}");
+        }
+        public static void Main(String[] args)
+        {
+            BankAccount account = new BankAccount("Татьяна", 1500);
+            account.Deposit(500);
+            account.Withdraw(50);
+            account.Withdraw(200);
+
+        }
+    }
+}
---
+++ task_2_/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task_2_")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task_2_")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a97d5282-2719-4d56-b054-cf22edd67a2a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
